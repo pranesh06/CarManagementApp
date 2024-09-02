0f965c1fcf1d247e102a6c1d94fdbb8020c5fc8f
Pranesh.T <Pranesh.T@INESL81030>
2024-09-02 11:01:08 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarManagementApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
